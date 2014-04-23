287c08a02b56f669e25079fed5617d9099346bf1
Curtis Lacy <curtis@engine.co>
2014-04-23 12:41:29 -0400

    Added an asynchronous balance query.

--- public/js/slimwallet.js
+++ public/js/slimwallet.js
@@ -1,8 +1,15 @@
-var SlimWalletData = Backbone.Model.extend( {
+var AddressData = Backbone.Model.extend( {
 	// address: the bitcoin address we're viewing.
 });
+var BalanceData = Backbone.Model.extend( {
+	// Map currency ID to balance.
+});
 
-var slimWalletData = new SlimWalletData();
+var slimWalletData = {
+	"addressData": new AddressData(),
+	"balances": new BalanceData()
+}
+var workers = {};
 
 $( function() {
 
@@ -13,7 +20,7 @@ $( function() {
 
 	// This HAS to happen after the rest of the UI syncs up, since address changes
 	// after this point will redirect the browser.
-	slimWalletData.on( 'change:address', function( data ) {
+	slimWalletData.addressData.on( 'change:address', function( data ) {
 		window.location.href = '/addr/' + data.changed.address;
 	});
 
@@ -21,32 +28,64 @@ $( function() {
 
 // Attach listeners to UI components that set data in the model.
 // DO NOT CHANGE ANY UI COMPONENT VALUES HERE!
-function attachModelSetters( dataModel ) {
+function attachModelSetters( data ) {
 	$( '#address-search' ).submit(function(e) {
 		e.preventDefault();
-		dataModel.set( { address: $( '#address-search input' ).val() });
+		data.addressData.set( { address: $( '#address-search input' ).val() });
 	});
 
 };
 
 // Attach listeners to the data model that populate the UI based on its data.
 // DO NO READ FROM ANY UI COMPONENTS HERE!
-function attachModelListeners( dataModel ) {
+function attachModelListeners( data ) {
 
 	// Update the display of the currently viewed address in the UI.
-	dataModel.on( 'change:address', function( data ) {
+	data.addressData.on( 'change:address', function( data ) {
 		$( '#address-search input' ).attr( 'placeholder', data.changed.address );
 		$( '#address-display' ).text( data.changed.address );
 		window.document.title = 'SlimWallet - ' + data.changed.address;
 	} );
 
+	workers[ 'balanceQuery' ] = new BalanceQueryWorker( data );
 };
 
 // Populate the data model as needed.
-function initModelData( dataModel ) {
+function initModelData( data ) {
 
 	var addressMatch = window.location.href.match( /\/addr\/([a-zA-Z0-9]+)$/ );
 	if( addressMatch )
-		dataModel.set( { address: addressMatch[1] } );
+		data.addressData.set( { address: addressMatch[1] } );
 
 };
+
+// Actually recovers balances asyncronously.
+function BalanceQueryWorker( data ) {
+	var self = this;
+	this.addressModel = data.addressData;
+	this.balances = data.balances;
+	this.addressModel.on( 'change:address', function( data ) {
+		self.setAddress( data.changed.address );
+	} );
+}
+BalanceQueryWorker.prototype.setAddress = function( newAddress ) {
+	clearTimeout( this.loop );
+	this.loop = setTimeout( this.getBalances.bind( this ) );
+}
+BalanceQueryWorker.prototype.getBalances = function() {
+	var self = this;
+	var originalAddress = this.addressModel.get( 'address' );	$.getJSON( 'http://btc.blockr.io/api/v1/address/info/' + originalAddress, 
+		function( response ) {
+			if( originalAddress == self.addressModel.get( 'address' ))
+			{
+				if( response.code == 200 )
+				{
+					self.balances.set( { 'bitcoin': response.data.balance } );
+					console.log( 'Balance is: ' + self.balances.get( 'bitcoin' ) + ' BTC' );
+				}
+			}
+		} 
+	);
+
+	this.loop = setTimeout( this.getBalances.bind( this ), 30000 );
+}
