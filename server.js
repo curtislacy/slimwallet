1d947bff5079e450649edcbf52681fc8ccd12e11
Curtis Lacy <curtis@engine.co>
2014-04-23 22:34:08 -0400

    First cut of things that are needed to make this run in heroku.

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get( '/', function( req, res ) {
 });
 
 
-
-app.listen(3000);
-console.log('Listening on port 3000...');
+var port = Number( process.env.PORT || 3000 );
+app.listen( port, function() {
+	console.log('Listening on port ' + port + '...');	
+});
